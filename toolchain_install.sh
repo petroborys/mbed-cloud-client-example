74e1f375a0b68ae8818d838d9afaa45faf9f1c91
Petro Borys <petro.borys@gmail.com>
2019-01-22 19:14:50 +0200

    Update toolchain_install.sh

--- toolchain_install.sh
+++ toolchain_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-chmod +x mbed-ci/toolchain_install.sh
+#chmod +x mbed-ci/toolchain_install.sh
+chmod +x toolchain_install.sh
 . mbed-ci/toolchain_install.sh && linux_default_download
 
 $HOME/bin/arm-none-eabi-gcc --version
